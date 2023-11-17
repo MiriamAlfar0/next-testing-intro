6071148cf75065a770b527f78a9fd50bac7e9a93
miriam alfaro villca <miriam0alfaro098@gmail.com>
2023-11-17 18:52:41 -0400

    testing

---
+++ __test__/Home.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/app/page'
+
+describe('Home', () => {
+    it('should have Docs text', () => {
+        render(<Home />) // ARRANGE 
+
+        const myElem = screen.getByText('ALFARO-MIRIAM') // ACT 
+
+        expect(myElem).toBeInTheDocument() // ASSERT
+    })
+
+    it('should contain the text "information"', () => {
+        render(<Home />) // ARRANGE 
+
+        const myElem = screen.getByText(/information/i) // ACT 
+
+        expect(myElem).toBeInTheDocument() // ASSERT
+    })
+    it('should have a heading', () => {
+        render(<Home />) // ARRANGE 
+
+        const myElem = screen.getByRole('heading', {
+            name: 'Learn'
+        }) // ACT 
+
+        expect(myElem).toBeInTheDocument() // ASSERT
+    })
+
+    
+
+})
